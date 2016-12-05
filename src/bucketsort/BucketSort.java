0a4d5cd03161da0c5e9a89038acd5ca930cdb16f
evzha0751 <evanzhao77@hotmail.com>
2016-12-04 19:59:40 -0500

    new commit

--- src/bucketsort/BucketSort.java
+++ src/bucketsort/BucketSort.java
@@ -9,70 +9,56 @@ import java.util.*;
 public class BucketSort {
 
     public static void BucketSort(int[] a,int max,int min){
+        String listOutput = "";
         int bucketCount =10;
-        int size = max/10+1;
-        int length = max-min+1;
-        Integer[][] bucket = new Integer[bucketCount][length];  
+        int size = (max-min)/10+1;
+        //Creat a list of bucket 
+        ArrayList<ArrayList<Integer>> bucket = new ArrayList<>();
+        for(int i=0;i<bucketCount;i++)
+        bucket.add(new ArrayList<>());
+        //Add the numbers to the buckets
         for (int i=0; i<a.length; i++){
-            int x = a[i]/size; 
-            for (int j=0; j<length; j++){
-                if (bucket[x][j]==null){
-                    bucket[x][j]=a[i]-min;
-                    break;
-                }
-            }
+            int x = (a[i]-min)/size; 
+           bucket.get(x).add(a[i]);
         }
         System.out.println("");
-        for (int i=0;i<bucketCount;i++){
-            
-            
-            for (int j=1; j<length; j++){
-                if(bucket[i][j]==null){
-                    break;
-                }
-                int value = bucket[i][j];
-                int index=j;
-                while (index>0 && bucket[i][index-1]>value){
-                    bucket[i][index] = bucket[i][index-1];
-                    index--;
-                }
-                bucket[i][index] = value;
-            }
-            
-            
-            for(int j=0;j<length;j++){
-                if(bucket[i][j]==null){
-                    break;
-                }
-                    System.out.print(bucket[i][j]+min+" ");
-            }
-        }
         
+        //Sort each bucket and add to output
+        for (int i=0;i<bucketCount;i++){
+           Collections.sort(bucket.get(i));
+           for(int j=0;j<bucket.get(i).size();j++){
+               listOutput += ((bucket.get(i).get(j))+" ");
+           }
+        }
+        //Print out the output
+        System.out.println(listOutput);
         
     }
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int number,max= Integer.MIN_VALUE,min= Integer.MAX_VALUE;
-        System.out.println("Please enter the number of array");
+        System.out.println("Please enter the number of elements");
         number = input.nextInt();
         int[] num = new int[number];
         int inputNumber,x;
         
+        //Get thee max value and min value of the array
         for(int i=0;i<number;i++){
             inputNumber = input.nextInt();
             if(inputNumber>max){
                 max = inputNumber;
-            }else if(inputNumber<min){
+            }if(inputNumber<min){
                 min = inputNumber;
             }
             num[i]=inputNumber;
         }
         
         
-        
+        //Print out the array before sorted
         for(int i=0;i<num.length;i++){
             System.out.print(num[i]+" ");
         }
+        
         BucketSort(num,max,min);
     }
     
